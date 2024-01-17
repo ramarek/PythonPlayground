50846ba42072a9b1de56509b7054b75a0683861c
Robert Marek <r.a.marek@icloud.com>
2024-01-17 16:48:40 +0100

    add first lesson

---
+++ Hello World/HelloWorld.py
@@ -0,0 +1,15 @@
+print("Wlecome", "whats your name?")
+
+W = "Welcome  to the Playground"
+name = "TEST"
+print(W, name);
+l1 = 1
+l2 = 2
+l3 = 3
+w1 = "to day we know Working"
+w2 = "with Variables and "
+w3 = "Memo"
+w4 = "ry"
+print("Thats " ,l3-l2 , "lesson")
+print(w1 , w2 , w3 + w4)
+
