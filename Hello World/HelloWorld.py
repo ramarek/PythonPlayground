db1546b10e9c2551d6ff98a2f6bbccedf558171a
Robert Marek <r.a.marek@icloud.com>
2024-01-19 20:06:35 +0100

    update, addition of mathematical operations, simple corrections.

--- Hello World/HelloWorld.py
+++ Hello World/HelloWorld.py
@@ -6,10 +6,22 @@ print(W, name);
 l1 = 1
 l2 = 2
 l3 = 3
-w1 = "to day we know Working"
+
+w1 = "to day we Working"
 w2 = "with Variables and "
 w3 = "Memo"
 w4 = "ry"
+# workin on string
 print("Thats " ,l3-l2 , "lesson")
 print(w1 , w2 , w3 + w4)
+# working on numbers
+# comparing numbers
+print(l1==l2)
+print(l1>l2)
+print( l1+l2 != l3 )
+# match operation witch numbers
+print("7+3=",7 + 3 )
 
+print("10+5=", 10 - 5)
+print("5*3=", 5 * 3)
+print("10/2=",  10 / 2)
