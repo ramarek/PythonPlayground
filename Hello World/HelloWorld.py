d08136da9f2fdd5eda7390a0b1e6c06ae3be63ef
Robert Marek <r.a.marek@icloud.com>
2024-01-19 20:39:50 +0100

    update, addition of mathematical operations, simple corrections.

--- Hello World/HelloWorld.py
+++ Hello World/HelloWorld.py
@@ -1,16 +1,23 @@
-print("Wlecome", "whats your name?")
-
-W = "Welcome  to the Playground"
-name = "TEST"
-print(W, name);
+a = 7
+b = 3
 l1 = 1
 l2 = 2
 l3 = 3
-
-w1 = "to day we Working"
+W = "Welcome  to the Playground"
+name = "TEST"
+w1 = "To day we Working"
 w2 = "with Variables and "
 w3 = "Memo"
 w4 = "ry"
+
+print("Wlecome", "whats your name?")
+
+
+
+print(W, name);
+
+
+
 # workin on string
 print("Thats " ,l3-l2 , "lesson")
 print(w1 , w2 , w3 + w4)
@@ -20,8 +27,14 @@ print(l1==l2)
 print(l1>l2)
 print( l1+l2 != l3 )
 # match operation witch numbers
-print("7+3=",7 + 3 )
+print("7 + 3 =",7 + 3, "Addition (+)")
+print("10 + 5 =", 10 - 5, "Subtraction (-)")
+print("5 * 3 =", 5 * 3, "Multiplication (*)")
+print("10 / 2 =",  10 / 2, "Division (/)")
 
-print("10+5=", 10 - 5)
-print("5*3=", 5 * 3)
-print("10/2=",  10 / 2)
+floor_quotient = a // b
+print("7 // 3 =", floor_quotient, "Floor Division (//)")  # Output: 2 (integer division)
+remainder = a % b
+print("7 % 3 =",  remainder, "Modulus (%)")  # Output: 1 (remainder of division)
+power = a ** b
+print("7 ** 3 =", power, "Exponentiation (**)")  # Output: 8
